22a3cdfef0e5175236c23f00327959a1712f977a
Muhamad Novaldy Wiradharma <69621594+valdy24-code@users.noreply.github.com>
2021-08-19 21:32:16 +0700

    Create latihan.php

---
+++ latihan.php
@@ -0,0 +1,47 @@
+<h2>Daftar Mahasiswa</h2>
+<?php
+   include 'koneksi.php';
+   $db = new database(); 
+   $conn = $db->Connect();
+
+   $query = mysqli_query($conn, "select * from tbl_mahasiswa");
+   while($data = mysqli_fetch_array($query)){
+       echo $data['npm'].' '.$data['nama']; echo "<br>";
+   }
+
+   if(isset($_POST['insert']))
+   {
+        $query = mysqli_query($conn, "insert into tbl_mahasiswa values(
+            '".$_POST['npm']."',
+            '".$_POST['nama']."'
+            )");
+            header('location:latihan.php');
+        }
+    
+    if(isset($_POST['update']))
+    {
+        $npm = $_POST['npm'];
+        $nama = $_POST['nama'];
+        $query = mysqli_query($conn, "update tbl_mahasiswa set nama='$nama' where npm='$npm'");
+            header('location:latihan.php');
+    }
+
+    if(isset($_POST['delete']))
+    {
+        $npm = $_POST['npm'];
+        $nama = $_POST['nama'];
+        $query = mysqli_query($conn, "delete from tbl_mahasiswa where npm='$npm'");
+            header('location:latihan.php');
+    }
+?>
+
+<form action="" method="POST">
+    <input type="text" name="npm">
+    <input type="text" name="nama">
+    <input type="submit" name="insert" value="simpan">
+    <input type="submit" name="update" value="Update">
+</form>
+<form action="" method="POST">
+    <input type="text" name="npm">
+    <input type="submit" name="delete" value="delete">
+</form>
